64c9157e8817fffbac14348d579b6a4fcc47c8a6
maciel310 <maciel310@gmail.com>
2025-03-04 22:46:22 -0700

    Fix Null exception when no object is selected and improve dialog title.

--- ModEntry.cs
+++ ModEntry.cs
@@ -29,22 +29,27 @@ namespace GiveAnywhere
         }
 
         private void TriggerGiftFlow() {
-            bool? canGiveAsGift = Game1.player.ActiveObject?.canBeGivenAsGift();
-            NetBool? isQuestItem = Game1.player.ActiveObject?.questItem;
+            StardewValley.Object activeObject = Game1.player.ActiveObject;
+            if (activeObject == null) return;
+
+            bool? canGiveAsGift = activeObject.canBeGivenAsGift();
+            NetBool? isQuestItem = activeObject.questItem;
             if (canGiveAsGift.HasValue && canGiveAsGift.GetValueOrDefault() && (isQuestItem == null || !isQuestItem.Value) )
             {
                 Game1.player.Halt();
+
+                String dialogQuestion = $"Offer {activeObject.name} to which farmer?";
 
                 IEnumerable<Farmer> otherFarmers = Game1.getOnlineFarmers().Where(f => f.UniqueMultiplayerID != Game1.player.UniqueMultiplayerID);
                 Response[] farmersResponses = otherFarmers.Select(farmer => new Response(farmer.UniqueMultiplayerID.ToString(), farmer.Name))
                     .Append(new Response("", "Cancel"))
                     .ToArray();
-                Game1.player.currentLocation.createQuestionDialogue("Which farmer?", farmersResponses, delegate (Farmer _, string answer) {
+                Game1.player.currentLocation.createQuestionDialogue(dialogQuestion, farmersResponses, delegate (Farmer _, string answer) {
                     if (answer == "")
                     {
                         return;
                     }
-                    Game1.player.team.SendProposal(otherFarmers.Where(f => f.UniqueMultiplayerID.ToString() == answer).First(), ProposalType.Gift, Game1.player.ActiveObject?.getOne());
+                    Game1.player.team.SendProposal(otherFarmers.Where(f => f.UniqueMultiplayerID.ToString() == answer).First(), ProposalType.Gift, activeObject.getOne());
                     Game1.activeClickableMenu = new PendingProposalDialog();
                 });
             }
